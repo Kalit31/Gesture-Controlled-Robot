1d62ebafe4afce9b26238fa626552532d71481c5
Keshav Kabra <keshavkabra123@gmail.com>
2024-11-05 12:31:06 -0500

    Finetuning of heading

--- flask_server.py
+++ flask_server.py
@@ -8,6 +8,7 @@ import asyncio
 MAX_SPEED = 100
 INITIAL_NUMBER_OF_REQUESTS_TO_IGNORE = 30
 NUMBER_OF_REQUESTS_TO_CALIBRATE = INITIAL_NUMBER_OF_REQUESTS_TO_IGNORE + 10
+CONSTANT_ROTATION_SPEED = 3
 sys.path.append('/home/kinani/Desktop/sphero-sdk-raspberrypi-python')
 from sphero_sdk import SpheroRvrObserver
 rvr = SpheroRvrObserver()
@@ -15,6 +16,7 @@ rvr = SpheroRvrObserver()
 count = 0
 calibrated_yaw_sum = 0.0
 calibrated_yaw = 0.0
+previous_heading = 0
 
 app = Flask(__name__)
 
@@ -27,6 +29,7 @@ def receive_data():
     global count
     global calibrated_yaw_sum
     global calibrated_yaw
+    global previous_heading
     data = request.get_json()
     count += 1
     # data_dict = json.load(data)
@@ -39,6 +42,7 @@ def receive_data():
 
     if count == NUMBER_OF_REQUESTS_TO_CALIBRATE:
         calibrated_yaw = calibrated_yaw_sum/(NUMBER_OF_REQUESTS_TO_CALIBRATE-INITIAL_NUMBER_OF_REQUESTS_TO_IGNORE)
+        previous_heading = 0
         print(f"Calibrated Yaw value: {calibrated_yaw}")
         return '', 204  # 204 No Content
 
@@ -46,40 +50,63 @@ def receive_data():
     # return jsonify({"status": "success", "received": data})
     return '', 204  # 204 No Content
 
-def move(data):
-    global calibrated_yaw
+def calculate_speed(pitch):
     drive_fwd = 0
-    speed = 0
-    heading_value = float(data["yaw"])
-    pitch_value = float(data["pitch"])
-    print(f"Got pitch: {pitch_value}")
-    if pitch_value > 0:
+    if pitch > 0:
         drive_fwd = 1
-    abs_pitch = abs(pitch_value)
+
+    abs_pitch = abs(pitch)
 
     if (abs_pitch <= 25):
-        # speed = 0
-        return
+        speed = 0
+        return (speed, drive_fwd)
 
     if (abs_pitch >= 70 and abs_pitch <=90):
         speed = MAX_SPEED
     else:
         speed = int(((abs_pitch-25)/45)*(MAX_SPEED-1))
 
-    heading_value_norm = int((heading_value - calibrated_yaw)%360)
+    return (speed, drive_fwd)
 
-    # if drive_fwd:
-    #     drive_forward(speed, heading)
-    # else:
-    #     drive_backward(speed, heading)
-    print(f"Driving with speed:{speed}, flags:{drive_fwd}")
+def calculate_heading(yaw):
+    global calibrated_yaw
+    # global previous_heading
+    difference_in_heading = yaw - calibrated_yaw
+    intermidate_value = (difference_in_heading)%360
+    heading_value_norm = int((-1 * intermidate_value) % 360)
+
+    if (heading_value_norm > 335 or heading_value_norm < 25):
+        heading_value_norm = 0
+    else:
+        if heading_value_norm > 180:
+            heading_value_norm = 340
+        elif heading_value_norm < 180:
+            heading_value_norm = 20
+
+    return heading_value_norm
+
+def move(data):
+    global previous_heading
+    yaw_value = float(data["yaw"])
+    pitch_value = float(data["pitch"])
+    print(f"Got pitch: {pitch_value}")
+    (speed, drive_fwd) = calculate_speed(pitch_value)
+    heading = calculate_heading(yaw_value)
+
+    if (speed == 0 and heading > 0):
+        heading = (previous_heading + heading)%360
+        previous_heading = heading
+        speed = CONSTANT_ROTATION_SPEED
+    else:
+        heading = previous_heading
+
+    print(f"Driving with speed:{speed}, flags:{drive_fwd}, headin:{heading}")
     rvr.drive_with_heading(
         speed=speed,       # This is out of 255, where 255 corresponds to 2 m/s
-        heading=heading_value_norm,      # Valid heading values are 0-359
+        heading=heading,      # Valid heading values are 0-359
         # time_to_drive=time # Driving duration in seconds
         flags=drive_fwd
     )
-
 
 def drive_forward(speed, heading, time=0.3):
     print(f"Driving forward with speed {speed}")
