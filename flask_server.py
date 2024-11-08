8378745b44b308556fbb207f87137271105ecff5
Keshav Kabra <keshavkabra123@gmail.com>
2024-11-08 10:45:33 -0500

    Add support for roll based rotation

--- flask_server.py
+++ flask_server.py
@@ -5,7 +5,7 @@ import sys
 import time
 import asyncio
 
-MAX_SPEED = 100
+MAX_SPEED = 140
 INITIAL_NUMBER_OF_REQUESTS_TO_IGNORE = 30
 NUMBER_OF_REQUESTS_TO_CALIBRATE = INITIAL_NUMBER_OF_REQUESTS_TO_IGNORE + 10
 CONSTANT_ROTATION_SPEED = 3
@@ -36,14 +36,14 @@ def receive_data():
     print("Received data:", data)
     if count < INITIAL_NUMBER_OF_REQUESTS_TO_IGNORE:
         return '', 204  # 204 No Content
-    elif count < NUMBER_OF_REQUESTS_TO_CALIBRATE:
-        calibrated_yaw_sum += data["yaw"]
-        return '', 204  # 204 No Content
+    # elif count < NUMBER_OF_REQUESTS_TO_CALIBRATE:
+    #     calibrated_yaw_sum += data["yaw"]
+    #     return '', 204  # 204 No Content
 
-    if count == NUMBER_OF_REQUESTS_TO_CALIBRATE:
-        calibrated_yaw = calibrated_yaw_sum/(NUMBER_OF_REQUESTS_TO_CALIBRATE-INITIAL_NUMBER_OF_REQUESTS_TO_IGNORE)
+    if count == INITIAL_NUMBER_OF_REQUESTS_TO_IGNORE:
+        # calibrated_yaw = calibrated_yaw_sum/(NUMBER_OF_REQUESTS_TO_CALIBRATE-INITIAL_NUMBER_OF_REQUESTS_TO_IGNORE)
         previous_heading = 0
-        print(f"Calibrated Yaw value: {calibrated_yaw}")
+        # print(f"Calibrated Yaw value: {calibrated_yaw}")
         return '', 204  # 204 No Content
 
     move(data)
@@ -85,13 +85,27 @@ def calculate_heading(yaw):
 
     return heading_value_norm
 
+def calculate_heading_using_roll(roll):
+    heading_value_norm = int((360-roll)%360)
+
+    if (heading_value_norm > 320 or heading_value_norm < 40):
+        heading_value_norm = 0
+    else:
+        if heading_value_norm > 180:
+            heading_value_norm = 340
+        elif heading_value_norm < 180:
+            heading_value_norm = 20
+    return heading_value_norm
+
 def move(data):
     global previous_heading
     yaw_value = float(data["yaw"])
     pitch_value = float(data["pitch"])
+    roll_value = float(data["roll"])
     print(f"Got pitch: {pitch_value}")
     (speed, drive_fwd) = calculate_speed(pitch_value)
-    heading = calculate_heading(yaw_value)
+    # heading = calculate_heading(yaw_value)
+    heading = calculate_heading_using_roll(roll_value)
 
     if (speed == 0 and heading > 0):
         heading = (previous_heading + heading)%360
@@ -107,25 +121,6 @@ def move(data):
         # time_to_drive=time # Driving duration in seconds
         flags=drive_fwd
     )
-
-def drive_forward(speed, heading, time=0.3):
-    print(f"Driving forward with speed {speed}")
-    rvr.drive_control.drive_forward_seconds(
-        speed=speed,       # This is out of 255, where 255 corresponds to 2 m/s
-        heading=heading,      # Valid heading values are 0-359
-        time_to_drive=time # Driving duration in seconds
-    )
-    print("Driving forward done")
-
-def drive_backward(speed, heading, time=0.3):
-    print(f"Driving backward with speed {speed}")
-    rvr.drive_control.drive_backward_seconds(
-        speed=speed,       # This is out of 255, where 255 corresponds to 2 m/s
-        heading=heading,      # Valid heading values are 0-359
-        time_to_drive=time # Driving duration in seconds
-    )
-    print("Driving backward done")
-
 
 def sphero_setup():
     # Make sure RVR is awake and ready to receive commands
